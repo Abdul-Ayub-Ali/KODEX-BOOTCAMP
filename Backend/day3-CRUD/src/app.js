1d778f935cfa16746c8b67a2462e15a0dd69f0e6
Faceless-Abdul <abdulayubali1224@gmail.com>
2026-05-03 13:07:06 +0530

    Update file app.js  with some validation

--- Backend/day3-CRUD/src/app.js
+++ Backend/day3-CRUD/src/app.js
@@ -1,8 +1,8 @@
-//Here i am creating server 
+//Here i am creating isstance of server  using express
 const express = require('express');
 const app = express();
 app.use(express.json())
-//From here on i am writing the logics from my CRUD
+//From here on i am writing the API's for my CRUD
 
 //This is the users data
 const users = [];
@@ -27,14 +27,17 @@ app.get('/getusers', (req, res) => {
 //Update (Patch)
 app.patch('/users/update/:index', (req, res) => { // Removed /update from URL
     const { index } = req.params;
-    const { age } = req.body;
 
     if (!users[index]) {
         return res.status(404).json({ message: "User not found at this index" });
     }
 
-    users[index].age = age;
-    return res.status(200).json({ message: "User age updated successfully" });
+    let updatedData = req.body;
+    users[index] = {...users[index], ...updatedData}
+    return res.status(201).json({
+        message: 'User Updated',
+        users
+    });
 });
 
 //Delete
