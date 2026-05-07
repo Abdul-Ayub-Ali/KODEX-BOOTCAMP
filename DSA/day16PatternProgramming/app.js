bc4a91bb8ffa92a77193fdae61f39e492b689e29
Faceless-Abdul <abdulayubali1224@gmail.com>
2026-05-07 12:17:29 +0530

    Learned Pattern Programming in depth

--- DSA/day16PatternProgramming/app.js
+++ DSA/day16PatternProgramming/app.js
@@ -1,5 +1,5 @@
 //Day16
-//Q1- Print Right Triangle-Star Pattern
+//Q1- Right Triangle-Star Pattern
 
 /*const prompt = require('prompt-sync')(); 
 
@@ -11,9 +11,9 @@ for (let i = 1; i <= n; i++) {
     console.log();
 }*/
 
-//Q2- Print Right Triangle-Number Pattern
+//Q2- Right Triangle-Number Pattern
 
-const prompt = require('prompt-sync')(); 
+/*const prompt = require('prompt-sync')(); 
 
 const n = Number(prompt('Enter your number: '));
 for (let i = 1; i <= n; i++) {
@@ -21,4 +21,79 @@ for (let i = 1; i <= n; i++) {
         process.stdout.write(`${j} `);
     }
     console.log();
-}
+}*/
+
+//Q3- Right Triangle-Alphabet Pattern
+
+
+/*const prompt = require('prompt-sync')(); 
+
+const n = Number(prompt('Enter your number: '));
+for (let i  = 0; i  < n; i++) {
+    for (let j = 0; j <=i; j++) {
+       process.stdout.write(String.fromCharCode(j+65)+" ");
+    };
+    console.log();
+};*/
+
+
+//Q4- Inverted Right Triangle-Star Pattern
+
+/*const prompt = require('prompt-sync')(); 
+
+const n = Number(prompt('Enter your number: '));
+for (let i = 1; i <= n; i++) {
+    for (let j = n; j >= i; j--) {
+        process.stdout.write(`* `);
+    };
+    console.log()
+};*/
+
+
+//Q5- Mirrored Right Triangle-Star Pattern
+
+/*const prompt = require('prompt-sync')(); 
+
+const n = Number(prompt('Enter your number: '));
+
+for (let i = 1;i<=n; i++) {
+    process.stdout.write("  ".repeat(n-i));
+    for (let j = 1; j <=i; j++) {
+        process.stdout.write("* ");
+    };
+    console.log();
+};*/
+
+//Q6-Print a V Shaped Pattern
+
+
+/*const prompt = require('prompt-sync')(); 
+
+const n = Number(prompt('Enter your number: '));
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= (n*2)-1; j++) {
+        if (i===j || i+j===n*2) {
+            process.stdout.write("*")
+        }
+        else process.stdout.write(" ")
+    }
+    console.log()
+}*/
+
+
+//Q7-Print an X-Shape Pattern
+
+/*const prompt = require('prompt-sync')(); 
+
+const n = Number(prompt('Enter your number: '));
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        if (i===j || i+j===n+1) {
+            process.stdout.write("*")
+        }
+        else process.stdout.write(" ")
+    }
+    console.log()
+}*/
