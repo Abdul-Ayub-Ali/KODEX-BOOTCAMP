0bd62e346f10701c554e333e8d7fb86b1ce8cd1b
Faceless-Abdul <abdulayubali1224@gmail.com>
2026-05-12 19:07:33 +0530

    understand the working of react useState Hook

---
+++ frontend/ReactJS/day8/understanding-UseState/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+const App = () => {
+
+  let [count, setCount] = useState(0);
+  const increment = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+  return (
+    <div className="h-screen">
+      <h1>count : {count} {num}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
